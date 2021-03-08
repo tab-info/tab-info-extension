ab93a726d0d19cd48e59aee40f226b4fc19b6810
Mike North <michael.l.north@gmail.com>
2021-03-07 17:00:56 -0800

    remove (now) irrelevant test

--- tests/unit/background/icon-color-updates.test.js
+++ tests/unit/background/icon-color-updates.test.js
@@ -13,10 +13,7 @@ const FALLBACK_COLOR_UINT8_CLAMPED_ARRAY = makeIconFromColor(
   FALLBACK_TAB_COLOR,
   TOOLBAR_ICON_SQUARE_SIZE
 ).data;
-const DISABLED_COLOR_UINT8_CLAMPED_ARRAY = makeIconFromColor(
-  DISABLED_TAB_COLOR,
-  TOOLBAR_ICON_SQUARE_SIZE
-).data;
+
 
 q_module('background process icon-update tests', () => {
 
@@ -154,71 +151,5 @@ q_module('background process icon-update tests', () => {
       }
     );
   });
-  q_test('responds to DISABLED content_script_ready appropriately', async (assert) => {
-    assert.expect(11);
-    function addListener(cb) {
-      setTimeout(() => {
-        function rcvResponse(arg) {
-          assert.ok(true, 'sendResponse was called upon receipt of message');
-          assert.equal(arg, 'ok', 'sendResponse was called with correct argument');
-        }
-        /** @type {import('../../../lib/types').ContentScriptReadyMessage} */
-        const msg = {
-          key: 'content_script_ready',
-          pageInfo: {
-            enabled: false,
-            tabInfo: {
-              pageTitle: 'my page title',
-              pageUrl: 'file:///',
-              buttonColor: '#f0f',
-              pageDescription: 'my page description',
-            },
-          },
-        };
-        cb(msg, { tab: { id: -1 } }, rcvResponse);
-      }, 0);
-      assert.ok(true, 'onMessage.addListener called');
-    }
 
-    function setIcon({ tabId, imageData }) {
-      assert.ok(true, 'chrome.pageAction.setIcon was called upon receipt of message');
-      assert.equal(tabId, -1, 'chrome.pageAction.setIcon was called with correct tabId');
-      assert.equal(
-        imageData.width,
-        TOOLBAR_ICON_SQUARE_SIZE,
-        'chrome.pageAction.setIcon was called with correct image width'
-      );
-      assert.equal(
-        imageData.height,
-        TOOLBAR_ICON_SQUARE_SIZE,
-        'chrome.pageAction.setIcon was called with correct image height'
-      );
-      assert.equal(
-        imageData.data.byteLength,
-        4 * TOOLBAR_ICON_SQUARE_SIZE * TOOLBAR_ICON_SQUARE_SIZE,
-        'chrome.pageAction.setIcon was called with correct image buffer length'
-      );
-      assert.deepEqual(
-        [...imageData.data],
-        [...DISABLED_COLOR_UINT8_CLAMPED_ARRAY],
-        'Icon pixels are correct'
-      );
-    }
-    function show(tabId, cb) {
-      assert.ok(false, 'chrome.pageAction.hide should not be called upon receipt of message');
-    }
-    function hide(tabId, cb) {
-      assert.ok(true, 'chrome.pageAction.hide was called upon receipt of message');
-      assert.equal(tabId, -1, 'chrome.pageAction.hide was passed the correct tabId');
-      cb();
-    }
-    setupMessageListeners(
-      { addListener },
-      {
-        show,
-        hide,
-        setIcon,
-      }
-    );
-  });
 });
